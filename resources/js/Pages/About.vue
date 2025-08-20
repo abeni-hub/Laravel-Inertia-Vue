56c36ebf74b8866c454e2d7f1976d0f306d0cfcf
abeni <abeni@email.com>
2025-08-20 15:33:18 -0700

    add about us page with shortcut

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+  defineProps({
+    user:String
+   })
+</script>
+<template>
+    <div>
+      <h1>Welcome to the About Page {{ user }}</h1>
+        <p>This is the home page of your application.</p>
+    </div>
+ </template>      
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-import './bootstrap';
-import '../css/app.css';
-import '../css/tailwind.css';
+import './bootstrap'
+import '../css/app.css'
+// import '../css/tailwind.css'
 
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
