2f45646abdd0a1ebe491c494946101011cb5fc79
abeni <abeni@email.com>
2025-08-21 12:51:41 -0700

    add layout

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header class="bg-gray-800 text-white p-4">
+        <nav class="flex justify-between items-center justify between p-4 
+        max-w-screen-lg mx-auto">
+
+           <div class="space-x-6">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+           </div>
+        </nav>
+    </header>
+    <main class="p-4">
+        <slot></slot>
+    </main>
+  </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,14 @@
+<script setup>
+import Layout from "../Layouts/Layout.vue"; 
+
+defineOptions({layout: Layout});
+</script>
+
+
 <template>
-    <div>
-      <h>Welcome to the Home Page</h>
-        <p>This is the home page of your application.</p>
-    </div>
+  
+    <h1 class="text-2xl font-bold mb-4">Welcome to the Home Page</h1>
+    <p>This is the home page content.</p>
+
+
  </template>      
